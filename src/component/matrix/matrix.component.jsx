2a21e400bd918c168a74b932e2452449eb5e48d2
David Ramsdale <davidramsdale1991@gmail.com>
2019-09-01 12:17:34 +1000

    Created a multiplication table

---
+++ src/component/matrix/matrix.component.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './matrix.styles.css';
+
+class Matrix extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  
+            selectedNumber: ''
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this)
+    }
+
+    
+    handleInputChange(event) {
+        this.setState({ selectedNumber: event.target.value})
+    }
+    
+    renderTable() {
+        let size = this.state.selectedNumber;
+        let table = [];
+    
+        for (let i = 1; i <= size; i++) {
+            let children = []
+            for (let j = 1; j <= size; j++) {
+            children.push(<td className="element">{`${j * i}`}</td>)
+            }
+            table.push(<tr>{children}</tr>)
+        }
+        return table        
+    }
+
+    render() { 
+        return (
+            <div className="container">   
+                <input type="number" name="number" onChange={this.handleInputChange} id="input-field" />
+                <table>
+                    <tbody>
+                        {this.renderTable()}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
+ 
+export default Matrix;
